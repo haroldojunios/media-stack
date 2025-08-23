e446b2ce4b94adf0394b75b5acf20c38ee0577c9
Haroldo Júnio <haroldojunios@gmail.com>
2025-08-23 11:37:22 -0300

    Initial commit

---
+++ backup/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+NOW=$(date +%Y%m%dT%H%M%S)
+DEST="/backups/media-stack-$NOW.tar.lrz"
+
+echo "[INFO] Starting backup at $(date)"
+
+# Compress entire docker folder but exclude the local backup storage
+cd /data
+tar -cf - . --exclude='./backups' | lrzip -o "$DEST"
+
+echo "[INFO] Backup created: $DEST"
+
+# Upload to Google Drive
+rclone copy "$DEST" drive:/media-stack-backups
+
+# Local cleanup (older than 7 days)
+find /backups -type f -mtime +7 -delete
+
+# Remote cleanup (older than 7 days)
+rclone delete drive:/media-stack-backups --min-age 7d
+
+echo "[INFO] Backup finished at $(date)"
